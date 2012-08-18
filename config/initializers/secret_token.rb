2b97df24145036b26320c69cff301311588cc806
Sasha Koss <koss@nocorp.me>
2012-08-18 18:17:42 +0700

    Push new rails application code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pushbio::Application.config.secret_token = '7bbd81801d0a88fdbe9352a010741dafc09a4289e969667b9f9b1df0bb4a09928e0fdefe09b2b9b16fd2ab7375090fbf79c9870c92e446d156d1d025f14f82b4'
